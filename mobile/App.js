4bc9c1071f6065d4e4ce2fc589eb319ae46f67c5
cuucas <lucasguima98@gmail.com>
2020-03-28 13:01:56 -0300

    mobile

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import Routes from './src/routes';
+import { Text, View } from 'react-native';
+import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
+
